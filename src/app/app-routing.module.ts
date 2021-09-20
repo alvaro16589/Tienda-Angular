203a00ab5f81576c4b1544f2a7db655f8e8a4967
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2021-09-19 22:02:57 -0400

    agregando un Guardian ng g g admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent} from './layout/layout.component';
+import { AdminGuard } from './admin.guard';
 const routes: Routes = [
   {
     path: '',
@@ -33,6 +34,7 @@ const routes: Routes = [
       },
       {
         path: 'contact',
+        canActivate: [AdminGuard],
         loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
       },
     ]
